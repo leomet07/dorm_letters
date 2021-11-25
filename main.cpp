74613473a153c75149d7701314c83bf7a4b424ac
leomet07 <leomet07@gmail.com>
2021-11-25 10:55:14 -0500

    refactor

--- main.cpp
+++ main.cpp
@@ -27,45 +27,17 @@ int main() {
         dormRoomInfo[i] = getLong();
     }
 
-    /*
-    int dorm_num = 3;
-    int test_num = 6;
-
-    int dormRoomInfo[] = {10, 15, 12};
-    int testCases[] =  { 1 , 9 , 12, 23, 26, 37};
-
-    for (int i = 0; i < test_num; i++){
-        int l = testCases[i];
-        cout << "L: " << l << endl;
-        int sim = l * 1;
-        int dorm = 0; 
-        while (sim > 0){
-            sim -= dormRoomInfo[dorm];
-            dorm ++;
-        }
-
-        int room = dormRoomInfo[dorm -1] + sim;
-        cout << "Sim: " << sim << " Dorm: " << dorm << " Room: " << room << endl;
-
-    }
-    */
     for (int i = 0; i < test_num; i++){
         long l = getLong();
 
-        // cout << "L: " << l << endl;
-        long sim = l * 1;
-
         long dorm = 0; 
-        while (sim > 0){
-            sim -= dormRoomInfo[dorm];
-            
+        while (l > 0){
+            l -= dormRoomInfo[dorm];
             dorm ++;
         }
 
-        long room = dormRoomInfo[dorm -1] + sim;
-        // cout << "Sim: " << sim << " Dorm: " << dorm << " Room: " << room << endl;
+        long room = dormRoomInfo[dorm -1] + l;
         cout << dorm << " " << room << endl;
-
 
     }
 
